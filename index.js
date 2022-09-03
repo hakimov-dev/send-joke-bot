b0e86d02865ae5676d2d2cfbb35296744b867f90
Muhammadamin <webhacker0100@gmail.com>
2022-09-03 16:28:12 +0500

    Deleted exes variable

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 const env = require('dotenv').config({path: './.env'});
-var codeRun = true
 
 async function runBot(){
     const { data } = await axios.get(`${env.parsed.TELEGRAM_API}${env.parsed.BOT_TOKEN}/getUpdates`)
