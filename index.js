82a88295d1efad85803af1a01a53a9c7763a05e1
Muhammadamin <webhacker0100@gmail.com>
2022-09-03 16:01:03 +0500

    Created new data variable and added request

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 const env = require('dotenv').config({path: './.env'});
 
 async function runBot(){
-    
+    const { data } = await axios.get(`${env.parsed.TELEGRAM_API}${env.parsed.BOT_TOKEN}/getUpdates`)
 }
