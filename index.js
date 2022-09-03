934e45d06489553954fcfa0794ef5fd808afed8d
Muhammadamin <webhacker0100@gmail.com>
2022-09-03 18:32:14 +0500

    Update

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ var XMLHttpRequest = require('xhr2');
 async function runBot(){
     try{
      const { data } = await axios.get(env.parsed.JOKE_API)
-     console.log(data)
+     const message = `${data.category} %0A %0A ${data.joke} %0A %0A Jokes: @jokes_by_robot`
+      sendMessage(message)
     }catch(error){
         console.log(error)
     }
 }
 
 function sendMessage(message){
-    var url = `${env.parsed.TELEGRAM_API}${env.parsed.BOT_TOKEN}/sendMessage?chat_id=${env.parsed.CHANEL_ID}&text=${message}&parse_mode=html`
+    var url = `${env.parsed.TELEGRAM_API}${env.parsed.BOT_TOKEN}/sendMessage?chat_id=${env.parsed.CHANEL_ID}&text=${message}`
 
     let oReq = new XMLHttpRequest();
          oReq.open("GET", url, true);
