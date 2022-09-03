d518ff66e2890fc1be238116e24ceb84756b208b
Muhammadamin <webhacker0100@gmail.com>
2022-09-03 16:27:45 +0500

    Created new codeRun variable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 const env = require('dotenv').config({path: './.env'});
+var codeRun = true
 
 async function runBot(){
     const { data } = await axios.get(`${env.parsed.TELEGRAM_API}${env.parsed.BOT_TOKEN}/getUpdates`)
